520c932c59e0bde28cc441204f72aca1fa9ff722
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-10-20 21:50:57 -0400

    add data and formatting
    create some seed data
    define root
    format dates and money

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,3 @@
+Time::DATE_FORMATS[:default] = "%B %d, %Y at %I:%M %p"
+
+# need to quit server after creating this file for it to take effect bc initializers run at beginning of app
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Event.create!([
+  {
+    name: "BugSmash",
+    location: "Denver",
+    price: 10.00,
+    description: "A fun evening of bug smashing",
+    starts_at: 10.days.from_now
+  },
+  {
+    name: "Hackathon",
+    location: "Austin",
+    price: 15.00,
+    description: "Hunker down at the Hackathon",
+    starts_at: 15.days.from_now
+  },
+  {
+    name: "Kata Camp",
+    location: "Dallas",
+    price: 75.00,
+    description: "Practice your craft kata style",
+    starts_at: 30.days.from_now
+  }
+  ])
