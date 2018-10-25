080c041c77248469b0b4d2620008e8b7f10b794f
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-10-25 10:38:54 -0400

    add lesson 11 part 2: submitting forms

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,18 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+    @event.update(event_params)
+    # # don't want to go to an update view template so we redirect
+    # redirect_to events_path(@event)
+    # # rails has a shortcut for this too
+    redirect_to @event
+  end
 end
