504329f8d06e681e54f427e98a7730c8612a5610
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-11-06 11:24:23 -0500

    add lesson 23: one-to-many forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,44 @@
 class RegistrationsController < ApplicationController
+  before_action :set_event
+
   def index
-    @event = Event.find(params[:event_id])
     @registrations = @event.registrations
+  end
+
+  def new
+    @registration = @event.registrations.new
+    # this is better than @registration = Registration.new because this way every new registration will be bound to an event by the event_id
+  end
+
+  def create
+    @registration = @event.registrations.new(registration_params)
+    if @registration.save
+      redirect_to event_registrations_path(@event), notice: "Thank you for registering."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @registration = Registration.find(params[:id])
+  end
+
+  def update
+    @registration = @event.registrations.update(registration_params)
+    if @registration.save
+      redirect_to event_registrations_path(@event), notice: "Thank you for registering."
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def registration_params
+    params.require(:registration).permit(:name, :email, :how_heard)
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
   end
 end
